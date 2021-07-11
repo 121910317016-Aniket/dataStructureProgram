c5c4915add6b62c42e22f7e4afa42b1639b0ef5d
121910317016 <74401205+121910317016-Aniket@users.noreply.github.com>
2021-07-11 11:19:49 +0530

    Update MultistageGraph.py

--- MultistageGraph.py
+++ MultistageGraph.py
@@ -2,7 +2,13 @@ def multistageGraph():
     stages=4
     cost=[]
     d=[]
-    path=[]
+    for i in range(9):
+        cost[i].append(0)
+        d[i].append(0)
+    path = []
+    for i in range(5):
+        path[i].append(0)
+
     c=[[0,0,0,0,0,0,0,0],
        [0,2,1,3,0,0,0,0],
        [0,0,0,0,2,3,0,0],
@@ -16,5 +22,14 @@ def multistageGraph():
         if i>=1:
             min=32757
             for k in range(i+1,n+1):
-                if(c[i][k]!=0 and c[i][k]+[k]<min):
+                if(c[i][k]!=0 and c[i][k]+cost[k]<min):
                     min=c[i][k]+cost[k]
+                    d[i]=k
+            cost[i]=min
+    path[1]=1
+    path[stage]=8
+    for i in range(2,stage):
+        p[i]=d[p[i-1]]
+    for i in range(stage+1):
+        print(p[i])
+multistageGraph()
