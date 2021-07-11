95a404911fc764953b35d8105185e98471064d35
121910317016-Aniket <121910317016@gitam.in>
2021-07-11 19:32:17 +0530

    new

--- AllPairShortestPath.py
+++ AllPairShortestPath.py
@@ -1,16 +1,13 @@
 def FloydWarshallAlgorithm(graph):
-    for i in range(4):
-        for j in range(4):
-            if j==i:
-                continue
-            for k in range(4):
-                if k==i:
-                    continue
-                if graph[i][k]+graph[j][i]<graph[j][k]:
-                    graph[j][k]=graph[i][k]+graph[j][i]
-
-
-
+    for k in range(4):
+        for i in range(4):
+            for j in range(4):
+                if i==j and k==i and k==j:
+                    continue 
+                else:
+                    if graph[i][j]>graph[i][k]+graph[k][j]:
+                        graph[i][j]=graph[i][k]+graph[k][j]
+                      
 INF=32678
 graph = [[0, 5, INF, 10],
          [INF, 0, 3, INF],
