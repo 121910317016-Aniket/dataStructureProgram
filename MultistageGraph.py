9805157ae3b07f44a28c0bb592d95152a886a67a
121910317016 <74401205+121910317016-Aniket@users.noreply.github.com>
2021-07-11 12:51:45 +0530

    Update MultistageGraph.py

--- MultistageGraph.py
+++ MultistageGraph.py
@@ -1,35 +1,35 @@
 def multistageGraph():
-    stages=4
+    n=8
+    stage=4
     cost=[]
     d=[]
-    for i in range(9):
-        cost[i].append(0)
-        d[i].append(0)
+    for i in range(8):
+        cost.append(0)
+        d.append(0)
     path = []
-    for i in range(5):
-        path[i].append(0)
+    for i in range(4):
+        path.append(0)
 
-    c=[[0,0,0,0,0,0,0,0],
-       [0,2,1,3,0,0,0,0],
-       [0,0,0,0,2,3,0,0],
-       [0,0,0,0,6,7,0,0],
-       [0,0,0,0,6,8,0,0],
-       [0,0,0,0,0,0,0,6],
-       [0,0,0,0,0,0,0,4],
-       [0,0,0,0,0,0,0,5]
-    ]
-    for i in range(n-1,-1):
-        if i>=1:
+    c=[[0, 1, 2, 5,0, 0, 0, 0],
+         [0, 0, 0, 0, 4, 11, 0, 0],
+         [0, 0, 0, 0, 9, 5, 16, 0],
+         [0, 0, 0, 0, 0, 0, 2, 0],
+         [0, 0, 0, 0, 0, 0, 0, 18],
+         [0, 0, 0, 0, 0, 0, 0, 13],
+         [0, 0, 0, 0, 0, 0, 0, 2]]
+    
+    for i in range(n-2,-1,-1):
+        if i>=0:
             min=32757
-            for k in range(i+1,n+1):
+            for k in range(i+1,n):
                 if(c[i][k]!=0 and c[i][k]+cost[k]<min):
                     min=c[i][k]+cost[k]
                     d[i]=k
             cost[i]=min
-    path[1]=1
-    path[stage]=8
-    for i in range(2,stage):
-        p[i]=d[p[i-1]]
-    for i in range(stage+1):
-        print(p[i])
+    path[0]=0
+    path[stage-1]=7
+    for i in range(1,stage):
+        path[i]=d[path[i-1]]
+    for i in range(stage):
+        print(path[i])
 multistageGraph()
